a9f6e7ae11a741406f6de6c263d93d6ffe0363f3
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-05-19 05:33:22 -0700

    Removed the static comments and integrate the  previouse functions

--- apps/frontend/src/functions/app.ts
+++ apps/frontend/src/functions/app.ts
@@ -1,8 +1,18 @@
+import generateCommentElement from "./generateCommentElement";
+import getComments from "./getComments";
 import setProfile from "./setProfile";
 
 async function app() {
   // Initilize a fake profile for the user
   await setProfile();
+  await getComments();
+  // Generating comments
+  const comments = await getComments();
+  const commentsElement: HTMLDivElement | null =
+    document.querySelector(".comments");
+  if (commentsElement) {
+    comments.map((item) => generateCommentElement(item, commentsElement));
+  }
 }
 
 export default app;
